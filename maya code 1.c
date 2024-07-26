7a1b184d683adff170bcaedf5ef43fb50ba4bf22
MAYA <mayapatel2975@gmail.com>
2024-07-26 11:23:02 +0530

    Add files via upload

---
+++ maya code 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+// include means to add something 
+// standard input output 
+// .h header file 
+// console
+
+int main()
+{  
+    printf( " welcome ");
+    printf( " my name is maya nareshbhai patel");
+    printf( " i am study in sbs ");
+}
+
