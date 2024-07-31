3d20376200de6da5358d4b0dc4e1dfb8a157117b
MAYA <mayapatel2975@gmail.com>
2024-07-31 22:41:51 +0530

    students marks.c

---
+++ Untitled1.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+ int main()
+  
+     
+	  {
+	  
+	    int toppers marks = 80;
+	     int failer marks = 40 ; 
+	  
+	  
+	  //check if student has passed or failed 
+	  
+	  if( toppers marks < failer marks )
+	   
+	   { print ("this student has fail in the exam. \n ");
+	   
+       }
+          else  { 
+              printf("this student has passed in the exam. \n");
+		  }
+	   	
+
+	 
+		 	
+	   
+	 ' return 0;'
+  	
+ }
+  	
+  	
+  	
+  	
+ ' THANKS FOR WATCHING :)'
+   MAYU738
+   
